941e40c017675f4030f9dfa9d70e24dca357098f
kirti18patel <kirti18patel@yahoo.com>
2021-05-24 22:40:42 -0400

    data fetched according to user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
 var searchButton = document.querySelector(".btn");
 var input = document.querySelector(".form-control");
-var result = document.querySelector("result");
+var result = document.querySelector(".result");
 
+var displayResult= function(data, userInput){
+    console.log(data, userInput);
+};
 var searchFun = function(event){
     event.preventDefault();
     var userInput=input.value;
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=34a53d9037f69833f5d3bd462bcecce3")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+ userInput +"&appid=34a53d9037f69833f5d3bd462bcecce3")
     .then( function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data);
-        // result.removeClass("hide");
+        displayResult(data, userInput);
+        result.classList.remove("hide");
     });
 };
 searchButton.addEventListener("click", searchFun)
