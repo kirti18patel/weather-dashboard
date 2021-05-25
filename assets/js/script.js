c54edb8be557b437b67b57d95458caef71fc6ff0
kirti18patel <kirti18patel@yahoo.com>
2021-05-25 01:07:55 -0400

    current weather cindition displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@ var input = document.querySelector(".form-control");
 
 var displayResult= function(data, userInput){
     console.log(data);
+    var fahrenheit = Math.round(((parseFloat(data.current.temp)-273.15)*1.8)+32); 
     $("#city-name").text(userInput);
-    $("#date").text("(" + moment().format("MM/DD/YYYY") + ")");
+    $("#date").text("(" + moment().format("l") + ")");
     $("#weather-icon").attr("src","http://openweathermap.org/img/w/"+data.current.weather[0].icon+".png");
-    $("#temp").text(data.current.temp + " °F");
+    $("#temp").text(fahrenheit + " °F");
     $("#wind").text(data.current.wind_speed + " MPH");
     $("#humidity").text(data.current.humidity + " %");
     $("#uv-index").text(data.current.uvi);
@@ -13,6 +14,10 @@ var displayResult= function(data, userInput){
 var searchFun = function(event){
     event.preventDefault();
     var userInput=input.value;
+    if(userInput==="" || !isNaN(userInput)){
+        alert("Please enter valid city name.");
+        return;
+    }
     userInput = userInput.charAt(0).toUpperCase() + userInput.substr(1).toLowerCase();
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+ userInput +"&appid=34a53d9037f69833f5d3bd462bcecce3")
     .then( function(response){
@@ -22,7 +27,7 @@ var searchFun = function(event){
         console.log(data);
         var lon= data.coord.lon;
         var lat= data.coord.lat;
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,daily&appid=34a53d9037f69833f5d3bd462bcecce3")
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,daily&appid=34a53d9037f69833f5d3bd462bcecce3")
         .then( function(result){
             return result.json();
         })
